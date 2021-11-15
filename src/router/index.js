45a5ddd31b34117b015324d4ca46ba8dc6605ecd
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-15 12:01:42 -0600

    Watch the  object in ViewProfile for changes to user_id

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router"; // Router is a Vue plug-in
 import Home from "@/components/Home";
 import About from "@/components/About";
+import ViewProfile from "@/components/ViewProfile";
 
 Vue.use(Router); // tells Vue to use the Router plug-in
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/about",
       name: "About",
       component: About
+    },
+    {
+      path: "/profile/:user_id",
+      name: "ViewProfile",
+      component: ViewProfile
     }
   ]
 });
