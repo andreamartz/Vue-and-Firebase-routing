ea0ec794179e17d31beff67e34a4246bb383eda5
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-15 11:08:40 -0600

    Add About component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router"; // Router is a Vue plug-in
-import HelloWorlHomed from "@/components/Home";
+import Home from "@/components/Home";
+import About from "@/components/About";
 
 Vue.use(Router); // tells Vue to use the Router plug-in
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      compontne: About
     }
   ]
 });
